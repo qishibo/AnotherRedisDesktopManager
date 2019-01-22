3c5bb09b55180b3ede8628846cae321d6f7faa47
qishibo <www.qii@qq.com>
2019-01-22 08:53:51 +0800

    dependence

--- redis-manager/config/index.js
+++ redis-manager/config/index.js
@@ -14,13 +14,13 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 9988, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
