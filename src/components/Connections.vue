d1cf10b0db8e92d05e9e0ce5aea3cb33f796e8c4
qishibo <www.qii@qq.com>
2019-02-28 16:10:51 +0800

    connection ad pool key

--- src/components/Connections.vue
+++ src/components/Connections.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <el-menu ref="connectionMenu" @open="openConnection" class="connection-menu" active-text-color="#ffd04b">
-      <el-submenu v-for="(item, index) of connections" :key="item.showName" :index="''+index">
+      <el-submenu v-for="(item, index) of connections" :key="item.showName" :index="getConnectionPoolKey(item) + '_' + index">
 
         <!-- connection item -->
         <template slot="title">
           <span slot="title" :title="item.showName" class="connection-name">{{item.showName}}</span>
           <!-- <i class="el-icon-search" @click.stop.prevent=""></i> -->
-          <i class="el-icon-edit-outline" @click.stop.prevent="showEditConnection(item, index)"></i>
+          <i class="el-icon-edit-outline" @click.stop.prevent="showEditConnection(item, getConnectionPoolKey(item))"></i>
           <i class="el-icon-delete" @click.stop.prevent="deleteConnection(item)"></i>
         </template>
 
         <el-form :inline="true" class="demo-form-inline" size="mini">
           <el-form-item>
             <!-- db index select -->
-            <el-select v-model="selectedDbIndex[index]" placeholder="DB" size="mini" @change="changeDb(index)">
+            <el-select v-model="selectedDbIndex[getConnectionPoolKey(item)]" placeholder="DB" size="mini" @change="changeDb(getConnectionPoolKey(item))">
               <el-option
                 v-for="index in dbs"
                 :key="index"
@@ -26,30 +26,30 @@
 
           <!-- search match -->
           <el-form-item>
-            <el-input v-model="searchMatch[index]" @keyup.enter.native="changeMatchMode(index)" placeholder="Enter To Search" suffix-icon="el-icon-search" size="mini"></el-input>
+            <el-input v-model="searchMatch[getConnectionPoolKey(item)]" @keyup.enter.native="changeMatchMode(getConnectionPoolKey(item))" placeholder="Enter To Search" suffix-icon="el-icon-search" size="mini"></el-input>
           </el-form-item>
 
         </el-form>
 
         <ul class="key-list">
-          <li class="key-item" v-for="key of keyList[index]" @click="clickKey(key, index)">{{key}}</li>
+          <li class="key-item" v-for="key of keyList[getConnectionPoolKey(item)]" @click="clickKey(key, getConnectionPoolKey(item))">{{key}}</li>
         </ul>
 
         <!-- page -->
         <div class="pagenation">
-          <el-button ref="pagePreButton" type="text" @click="pagePre(index)" :disabled="preButtonDisable(index)" size="mini" icon="el-icon-arrow-left"></el-button>
-          <input 
-            :value="getPageIndex(index)"
-            ref="pageIndexInput"
-            @click="$event.target.select()" 
-            @keyup.enter="jumpToPage(index, $event.target.value)" 
+          <el-button ref="pagePreButton" type="text" @click="pagePre(getConnectionPoolKey(item))" :disabled="preButtonDisable(getConnectionPoolKey(item))" size="mini" icon="el-icon-arrow-left"></el-button>
+          <input
+            :value="getPageIndex(getConnectionPoolKey(item))"
+            :ref="'pageIndexInput' + getConnectionPoolKey(item)"
+            @click="$event.target.select()"
+            @keyup.enter="jumpToPage(getConnectionPoolKey(item), $event.target.value)"
             class="page-jumper el-input__inner"
             type="number"
             min="1"
             step="1"
           >
           </input>
-          <el-button ref="pageNextButton" type="text" @click="pageNext(index)" :disabled="nextPageDisabled[index]" size="mini" icon="el-icon-arrow-right"></el-button>
+          <el-button ref="pageNextButton" type="text" @click="pageNext(getConnectionPoolKey(item))" :disabled="nextPageDisabled[getConnectionPoolKey(item)]" size="mini" icon="el-icon-arrow-right"></el-button>
         </div>
 
       </el-submenu>
@@ -116,23 +116,25 @@
       });
     },
     methods: {
-      openConnection(index) {
+      openConnection(openedMenuKey) {
         // get connection config
-        let connections = this.connections;
-        let connection = connections[index];
+        // openedMenuKey like 10.20.1.1336379_0
+        let menuIndex = openedMenuKey.split('_')[0];
+        let connectionIndex = openedMenuKey.split('_')[1];
+        let connection = this.connections[connectionIndex];
 
         if (!connection) {
           alert('open error');
           return;
         }
 
-        this.getDbIndex(index);
-        this.setGlobalConnection(index, connection);
+        this.getDbIndex(menuIndex);
+        this.setGlobalConnection(menuIndex, connection);
 
         // open status tab
-        if (!this.openedStatus[index]) {
+        if (!this.openedStatus[menuIndex]) {
           this.$bus.$emit('openStatus');
-          this.openedStatus[index] = true;
+          this.openedStatus[menuIndex] = true;
         }
 
         this.refreshKeyList();
@@ -213,14 +215,6 @@
 
         this.connections = connections;
       },
-      closeConnection(connection, menuIndex) {
-        console.log('closing...', connection , menuIndex);
-
-        let key = this.getConnectionPoolKey(connection);
-        delete this.connectionPool[key];
-
-        this.$refs.connectionMenu.close('' + menuIndex);
-      },
       closeAllConnection() {
         console.log('closing all...');
 
@@ -297,7 +291,7 @@
         if (cursorList[cursorListLength - 1] == '0') {
           return false;
         }
-        
+
 
         if (pageIndex < targetPage) {
           this.$set(this.pageIndex, menuIndex, ++pageIndex);
@@ -311,7 +305,7 @@
         targetPage = parseInt(targetPage);
 
         if (isNaN(targetPage) || targetPage <= 0) {
-          this.$refs.pageIndexInput[menuIndex].value = 1;
+          this.$refs['pageIndexInput' + menuIndex][0].value = 1;
           targetPage = 1;
         }
 
@@ -319,7 +313,7 @@
         let cursorListLength = this.scanCursorList[menuIndex].length;
 
         this.setGlobalConnection(menuIndex);
-        
+
         console.log('prepare to jump to',nowPage , targetPage);
 
         if (targetPage >= cursorListLength) {
@@ -329,7 +323,7 @@
 
           // last page
           if (recursiveResult === false) {
-            this.$refs.pageIndexInput[menuIndex].value = this.pageIndex[menuIndex];
+            this.$refs['pageIndexInput' + menuIndex][0].value = this.pageIndex[menuIndex];
 
             this.$message.error({
               message: this.$t('message.max_page_reached'),
@@ -361,10 +355,6 @@
         this.setGlobalConnection(menuIndex);
         this.refreshKeyList();
       },
-      getKeyList(connection) {
-        let key = this.getConnectionPoolKey(connection);
-
-      },
       getScanCursor(menuIndex) {
         if (this.scanCursorList[menuIndex] === undefined) {
           this.scanCursorList[menuIndex] = [0];
@@ -395,7 +385,6 @@
 
         let cursor = this.getScanCursor(menuIndex);
         let match = this.getMatchMode(menuIndex);
-
         let promise = client.scanAsync(cursor, 'MATCH', match, 'COUNT', this.keysPageSize).then(reply => {
           console.log(this.selectedDbIndex, cursor, match, 'scan result', reply);
 
