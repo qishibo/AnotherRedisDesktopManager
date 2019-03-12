8499acf0f4527ef2ce376dae1aa666a6d68d16f3
qishibo <www.qii@qq.com>
2019-03-12 10:05:12 +0800

    fix file path problems for electron and nwjs

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,9 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        // for font path import by css files
+        publicPath:"../../",
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
--- config/index.js
+++ config/index.js
@@ -43,7 +43,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // when not web conditon, such as index.html as main, relative path
+    assetsPublicPath: './',
 
     /**
      * Source Maps
