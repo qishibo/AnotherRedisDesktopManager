1a8ce5d02dab67f8ad6c50be245888c33c932ea4
Gavin <tianyu_0723@hotmail.com>
2019-09-29 14:40:11 +0800

    redis key 分组树形显示

--- src/util.js
+++ src/util.js
@@ -29,4 +29,52 @@ export default {
     e.preventDefault();
     require('electron').shell.openExternal(href);
   },
+  /**
+   * redis键数组分解为树形结构
+   * 
+   * @param {*} input 
+   * @param {*} split 
+   */
+  keysToTree(keys) {
+    function toTreeData(tree, path = '', init = true) {
+      // 当前迭代的节点路径
+      let currentIteratePath = path
+
+      return Object.keys(tree).map(function (label) {
+        var o = { label: label, redis: false };
+
+        if (init) {
+          path = label
+          currentIteratePath = path
+        } else if (Object.keys(tree).length > 1) {
+          currentIteratePath = path + ':' + label
+        } else {
+          path = path + ':' + label
+          currentIteratePath = path
+        }
+
+        if (Object.keys(tree[label]).length > 0) {
+          o.children = toTreeData(tree[label], path, false);
+        } else {
+          o.label = currentIteratePath
+          o.redis = true
+        }
+
+        return o;
+      });
+    }
+    // array 转换成 tree object
+    var tree = {};
+    keys.forEach(function (key) {
+      var currentNode = tree;
+      key.split(':').forEach(function (segment) {
+        if (currentNode[segment] === undefined) {
+          currentNode[segment] = {};
+        }
+        currentNode = currentNode[segment];
+      });
+    });
+
+    return toTreeData(tree)
+  }
 };
