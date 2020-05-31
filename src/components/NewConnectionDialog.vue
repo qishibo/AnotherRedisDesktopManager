0ec3eb373ac8be9d24734ecb402dd01042723787
Michael.Y.Ma <myg133@hotmail.com>
2020-05-31 16:17:55 +0800

    Add TLS feature

--- src/components/NewConnectionDialog.vue
+++ src/components/NewConnectionDialog.vue
@@ -1,13 +1,25 @@
 <template>
-  <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" :append-to-body='true'>
+  <el-dialog
+    :title="dialogTitle"
+    :visible.sync="dialogVisible"
+    :append-to-body="true"
+  >
     <!-- redis connection form -->
     <el-form :label-position="labelPosition" label-width="80px">
       <el-form-item label="Host">
-        <el-input v-model="connection.host" autocomplete="off" placeholder="127.0.0.1"></el-input>
+        <el-input
+          v-model="connection.host"
+          autocomplete="off"
+          placeholder="127.0.0.1"
+        ></el-input>
       </el-form-item>
 
       <el-form-item label="Port">
-        <el-input v-model="connection.port" autocomplete="off" placeholder="6379"></el-input>
+        <el-input
+          v-model="connection.port"
+          autocomplete="off"
+          placeholder="6379"
+        ></el-input>
       </el-form-item>
 
       <el-form-item label="Auth">
@@ -18,88 +30,120 @@
         <el-input v-model="connection.name" autocomplete="off"></el-input>
       </el-form-item>
 
+      <el-form-item label="SSL">
+        <el-checkbox v-model="connection.tls">Use SSL</el-checkbox>
+      </el-form-item>
+
       <el-form-item label="">
         <el-checkbox v-model="sshOptionsShow">SSH Tunnel</el-checkbox>
         <el-checkbox v-model="connection.cluster">Cluster</el-checkbox>
         <el-popover trigger="hover">
           <i slot="reference" class="el-icon-question"></i>
-          {{ $t('message.cluster_faq') }}
+          {{ $t("message.cluster_faq") }}
         </el-popover>
       </el-form-item>
 
       <!-- ssh connection form -->
       <el-form v-if="sshOptionsShow" v-show="sshOptionsShow" label-width="80px">
         <el-form-item label="Host">
-          <el-input v-model="connection.sshOptions.host" autocomplete="off"></el-input>
+          <el-input
+            v-model="connection.sshOptions.host"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
 
         <el-form-item label="Port">
-          <el-input v-model="connection.sshOptions.port" autocomplete="off"></el-input>
+          <el-input
+            v-model="connection.sshOptions.port"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
 
         <el-form-item label="Username">
-          <el-input v-model="connection.sshOptions.username" autocomplete="off"></el-input>
+          <el-input
+            v-model="connection.sshOptions.username"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
 
         <el-form-item label="Password">
-          <el-input v-model="connection.sshOptions.password" autocomplete="off"></el-input>
+          <el-input
+            v-model="connection.sshOptions.password"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
 
         <el-form-item label="PrivateKey">
           <el-tooltip effect="dark">
             <div slot="content" v-html="$t('message.private_key_faq')"></div>
-            <el-input v-if='connection.sshOptions.privatekey' v-model='connection.sshOptions.privatekey' clearable autocomplete="off" ></el-input>
-            <el-input v-else id='private-key-path' type='file' @change='changePrivateKey'></el-input>
+            <el-input
+              v-if="connection.sshOptions.privatekey"
+              v-model="connection.sshOptions.privatekey"
+              clearable
+              autocomplete="off"
+            ></el-input>
+            <el-input
+              v-else
+              id="private-key-path"
+              type="file"
+              @change="changePrivateKey"
+            ></el-input>
           </el-tooltip>
         </el-form-item>
       </el-form>
     </el-form>
 
     <div slot="footer" class="dialog-footer">
-      <el-button @click="dialogVisible = false">{{ $t('el.messagebox.cancel') }}</el-button>
-      <el-button type="primary" @click="editConnection">{{ $t('el.messagebox.confirm') }}</el-button>
+      <el-button @click="dialogVisible = false">{{
+        $t("el.messagebox.cancel")
+      }}</el-button>
+      <el-button type="primary" @click="editConnection">{{
+        $t("el.messagebox.confirm")
+      }}</el-button>
     </div>
   </el-dialog>
 </template>
 
 <script type="text/javascript">
-import storage from '@/storage';
+import storage from "@/storage";
 
 export default {
   data() {
     return {
       dialogVisible: false,
-      labelPosition: 'left',
-      oldKey: '',
+      labelPosition: "left",
+      oldKey: "",
       connection: {
-        host: '',
-        port: '',
-        auth: '',
-        name: '',
+        host: "",
+        port: "",
+        auth: "",
+        name: "",
+        tls: false,
         cluster: false,
         sshOptions: {
-          host: '',
+          host: "",
           port: 22,
-          username: '',
-          password: '',
-          privatekey: '',
+          username: "",
+          password: "",
+          privatekey: ""
         }
       },
-      sshOptionsShow: false,
-    }
+      sshOptionsShow: false
+    };
   },
-  props: ['config', 'editMode'],
+  props: ["config", "editMode"],
   computed: {
     dialogTitle() {
-      return this.editMode ? this.$t('message.edit_connection') :
-                              this.$t('message.new_connection')
-    },
+      return this.editMode
+        ? this.$t("message.edit_connection")
+        : this.$t("message.new_connection");
+    }
   },
   methods: {
     editConnection() {
       const config = JSON.parse(JSON.stringify(this.connection));
 
-      !config.host && (config.host = '127.0.0.1');
+      !config.host && (config.host = "127.0.0.1");
       !config.port && (config.port = 6379);
 
       if (!this.sshOptionsShow || !config.sshOptions.host) {
@@ -109,11 +153,11 @@ export default {
       storage.editConnectionByKey(config, this.oldKey);
 
       this.dialogVisible = false;
-      this.$emit('editConnectionFinished');
+      this.$emit("editConnectionFinished");
     },
     changePrivateKey() {
-      const path = document.getElementById('private-key-path').files[0].path;
-      this.$set(this.connection.sshOptions, 'privatekey', path);
+      const path = document.getElementById("private-key-path").files[0].path;
+      this.$set(this.connection.sshOptions, "privatekey", path);
     }
   },
   mounted() {
@@ -123,10 +167,11 @@ export default {
       this.oldKey = storage.getConnectionKey(this.config);
 
       this.sshOptionsShow = !!this.connection.sshOptions;
-      !this.connection.sshOptions && (this.connection.sshOptions = sshOptionsNew);
+      !this.connection.sshOptions &&
+        (this.connection.sshOptions = sshOptionsNew);
     }
 
     delete this.connection.connectionName;
-  },
-}
+  }
+};
 </script>
--- src/redisClient.js
+++ src/redisClient.js
@@ -1,8 +1,8 @@
-import Redis from 'ioredis';
-import tunnelssh from 'tunnel-ssh';
+import Redis from "ioredis";
+import tunnelssh from "tunnel-ssh";
 
 // fix ioredis hgetall key has been toString()
-Redis.Command.setReplyTransformer("hgetall", (result) => {
+Redis.Command.setReplyTransformer("hgetall", result => {
   let arr = [];
   for (let i = 0; i < result.length; i += 2) {
     arr.push([result[i], result[i + 1]]);
@@ -15,21 +15,24 @@ export default {
   createConnection(host, port, auth, config) {
     const options = {
       connectTimeout: 3000,
-      retryStrategy: (times) => {return this.retryStragety(times, {host, port})},
+      retryStrategy: times => {
+        return this.retryStragety(times, { host, port });
+      },
       enableReadyCheck: false,
       connectionName: config.connectionName ? config.connectionName : null,
       password: auth,
+      tls: config.tls
     };
 
     const clusterOptions = {
       connectionName: options.connectionName,
       enableReadyCheck: false,
-      redisOptions: options,
+      redisOptions: options
     };
 
-    const client = config.cluster ?
-                    new Redis.Cluster([{port, host}], clusterOptions) :
-                    new Redis(port, host, options);
+    const client = config.cluster
+      ? new Redis.Cluster([{ port, host }], clusterOptions)
+      : new Redis(port, host, options);
 
     return client;
   },
@@ -37,16 +40,18 @@ export default {
   createSSHConnection(sshOptions, host, port, auth, config) {
     const options = {
       connectTimeout: 3000,
-      retryStrategy: (times) => {return this.retryStragety(times, {host, port})},
+      retryStrategy: times => {
+        return this.retryStragety(times, { host, port });
+      },
       enableReadyCheck: false,
       connectionName: config.connectionName ? config.connectionName : null,
-      password: auth,
+      password: auth
     };
 
     const clusterOptions = {
       connectionName: options.connectionName,
       enableReadyCheck: false,
-      redisOptions: options,
+      redisOptions: options
     };
 
     const sshConfig = {
@@ -57,28 +62,31 @@ export default {
       readyTimeout: 2000,
       dstHost: host,
       dstPort: port,
-      localHost: '127.0.0.1',
+      localHost: "127.0.0.1",
       localPort: null,
-      privateKey: sshOptions.privatekey ?
-                  require('fs').readFileSync(sshOptions.privatekey) : '',
+      privateKey: sshOptions.privatekey
+        ? require("fs").readFileSync(sshOptions.privatekey)
+        : ""
     };
 
     const sshPromise = new Promise((resolve, reject) => {
-      var server = tunnelssh(sshConfig, function (error, server) {
+      var server = tunnelssh(sshConfig, function(error, server) {
         if (error) {
           reject(error);
-        }
-        else {
+        } else {
           const listenAddress = server.address();
-          const client = config.cluster ?
-                          new Redis.Cluster([{port: listenAddress.port, host: listenAddress.address}], clusterOptions) :
-                          new Redis(listenAddress.port, listenAddress.address, options);
+          const client = config.cluster
+            ? new Redis.Cluster(
+                [{ port: listenAddress.port, host: listenAddress.address }],
+                clusterOptions
+              )
+            : new Redis(listenAddress.port, listenAddress.address, options);
           resolve(client);
         }
       });
 
-      server.on('error', (error) => {
-        alert('SSH Connection Error: ' + error.message);
+      server.on("error", error => {
+        alert("SSH Connection Error: " + error.message);
         reject(error);
       });
     });
@@ -90,11 +98,15 @@ export default {
     const maxRetryTimes = 3;
 
     if (times >= maxRetryTimes) {
-      alert(`${connection.host}:${connection.port}\nToo Many Attempts To Reconnect. Please Check The Server Status!`);
+      alert(
+        `${connection.host}:${
+          connection.port
+        }\nToo Many Attempts To Reconnect. Please Check The Server Status!`
+      );
       return false;
     }
 
     // reconnect after
     return Math.min(times * 200, 1000);
-  },
+  }
 };
