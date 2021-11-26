b44740c85b367ea9dbeadffaece0b08f47f32607
qishibo <www.qii@qq.com>
2021-11-26 11:14:04 +0800

    optimize build size

--- src/components/JsonEditor.vue
+++ src/components/JsonEditor.vue
@@ -12,7 +12,8 @@
 </template>
 
 <script type="text/javascript">
-import * as monaco from 'monaco-editor';
+// import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 export default {
   data() {
