8c3528e95f6b8e3f84893b41eaacfa8fd160538d
qishibo <www.qii@qq.com>
2022-04-27 17:35:35 +0800

    fix #498,#850 More ssh algorithms support

--- src/redisClient.js
+++ src/redisClient.js
@@ -94,7 +94,16 @@ export default {
       localPort: null, // set null to use available port in local machine
       privateKey: this.getFileContent(sshOptions.privatekey, sshOptions.privatekeybookmark),
       passphrase: sshOptions.passphrase ? sshOptions.passphrase : undefined,
+      keepAlive: true,
       keepaliveInterval: 10000,
+      algorithms: {
+        cipher: ['aes128-gcm', 'aes128-gcm@openssh.com', 'aes256-gcm', 'aes256-gcm@openssh.com', 'aes128-ctr', 'aes192-ctr', 'aes256-ctr', 'chacha20-poly1305@openssh.com', '3des-cbc', 'aes256-cbc', 'aes192-cbc', 'aes128-cbc', 'arcfour256', 'arcfour128', 'arcfour', 'blowfish-cbc', 'cast128-cbc'],
+        compress: ['none', 'zlib@openssh.com', 'zlib'],
+        hmac: ['hmac-sha2-256-etm@openssh.com', 'hmac-sha2-512-etm@openssh.com', 'hmac-sha1-etm@openssh.com', 'hmac-sha2-256', 'hmac-sha2-512', 'hmac-sha1', 'hmac-md5', 'hmac-sha2-256-96', 'hmac-sha2-512-96', 'hmac-ripemd160', 'hmac-sha1-96', 'hmac-md5-96'],
+        kex: ['curve25519-sha256', 'curve25519-sha256@libssh.org', 'ecdh-sha2-nistp256', 'ecdh-sha2-nistp384', 'ecdh-sha2-nistp521', 'diffie-hellman-group-exchange-sha256', 'diffie-hellman-group14-sha256', 'diffie-hellman-group15-sha512', 'diffie-hellman-group16-sha512', 'diffie-hellman-group17-sha512', 'diffie-hellman-group18-sha512', 'diffie-hellman-group-exchange-sha1', 'diffie-hellman-group14-sha1', 'diffie-hellman-group1-sha1'],
+        serverHostKey: ['ssh-ed25519', 'ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521', 'rsa-sha2-512', 'rsa-sha2-256', 'ssh-rsa', 'ssh-dss'],
+      },
+      // debug: console.log
     };
 
     const configRaw = JSON.parse(JSON.stringify(config));
