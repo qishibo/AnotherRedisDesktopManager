f24af66482d0d3e26f5c9acdf7499c7fc1ce4656
djgrech <39998811+djgrech@users.noreply.github.com>
2023-03-07 04:10:53 +0100

    feat(viewers): added two viewers one deflating raw data, and another to display as JSON (#1039)
    * added two viewers one deflating raw data, and another to display as JSON
    
    * updated version
    
    * removed json+raw viewer
    
    ---------
    
    Co-authored-by: Derek Grech <derek.grech@river.tech>

---
+++ src/components/ViewerDeflateRaw.vue
@@ -0,0 +1,41 @@
+<template>
+  <JsonEditor ref='editor' :content='newContent' :readOnly='false'></JsonEditor>
+</template>
+
+<script type="text/javascript">
+import JsonEditor from '@/components/JsonEditor';
+
+const zlib = require('zlib');
+
+export default {
+  components: { JsonEditor },
+  props: ['content'],
+  computed: {
+    newContent() {
+      const { formatStr } = this;
+
+      if (typeof formatStr === 'string') {
+        if (this.$util.isJson(formatStr)) {
+          return JSON.parse(formatStr);
+        }
+
+        return formatStr;
+      }
+
+      return 'Zlib Deflate Parse Failed!';
+    },
+    formatStr() {
+      return this.$util.zippedToString(this.content, 'deflateRaw');
+    },
+  },
+  methods: {
+    getContent() {
+      const content = this.$refs.editor.getRawContent(true);
+      return zlib.deflateRawSync(content);
+    },
+    copyContent() {
+      return this.formatStr;
+    },
+  },
+};
+</script>
